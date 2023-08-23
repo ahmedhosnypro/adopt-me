82e929bee1804a21bb9c77622c541ba4472a2107
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-23 20:02:54 +0300

    network request

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+;
+src/App.jsx,c/0/c0e9e59a467e1546695bfa22746b302540797a4e
+C
+src/SearchParam.jsx,d/3/d3a9701ef0b62deefb0c1412b85ab9aa3c671798
+=
+src/style.css,1/9/199c81d8625cb1aaa85a72e49e4ad72a9fed6766
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet"
+import { createRoot } from "react-dom/client";
+// import Pet from "./Pet";
+import SearchParam from "./SearchParam";
 
 const App = () => {
-return(
-  <div>
-  <h1>Adopt-me</h1>
-  <Pet name="Luna" animal="Dog" breed = "Havanese" />
-  <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-  <Pet name="Doink" animal="Cat" breed = "Mixed" />
-</div>
-)};
+  return (
+    <div>
+      <h1>Adopt-me</h1>
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
+      <SearchParam />
+    </div>
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
