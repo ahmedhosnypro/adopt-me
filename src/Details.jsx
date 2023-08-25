e13f3f0a3a41150af523b7713b5a801790c660ec
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-25 18:12:50 +0300

    context

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,12 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Modal from "./Modal";
 
 const Details = () => {
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
+
   const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
@@ -34,7 +39,14 @@ const Details = () => {
             <div>
               <h1>Would you like to adopt {pet.name}?</h1>
               <div className="buttons">
-                <button>Yes</button>
+                <button
+                  onClick={() => {
+                    setAdoptedPet(pet);
+                    navigate("/");
+                  }}
+                >
+                  Yes
+                </button>
                 <button onClick={() => setShowModal(false)}>No</button>
               </div>
             </div>
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
 import fetchSearch from "./fetchSearch";
+import AdoptedPetContext from "./AdoptedPetContext";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
+  const [adoptedPet] =  useContext(AdoptedPetContext)
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
 
@@ -31,7 +33,13 @@ const SearchParams = () => {
           };
           setRequestParams(obj);
         }}
-      >
+      >{
+        adoptedPet ? (
+          <div className="pet image-container">
+          <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+        </div>
+        ) : null
+      }
         <label htmlFor="location">
           Location
           <input id="location" name="location" placeholder="Location" />
