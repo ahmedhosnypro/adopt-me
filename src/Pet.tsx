ef6421e52dae0fa9d3ab51bf8dd9d14acbc7889b
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-28 08:50:55 +0300

    migrate ts

--- src/Pet.tsx
+++ src/Pet.tsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
 
-const Pet = (props) => {
+import { Animal } from "./APIResponsesTypes";
+
+interface Iprops {
+  name: string;
+  animal: Animal;
+  breed: string;
+  images: string[];
+  location: string;
+  id: number;
+}
+
+const Pet = (props: Iprops) => {
   const { name, animal, breed, images, location, id } = props;
 
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
